5533943560397ac7e6db0a518d6a9cf4d742c45d
grimmr <grimms767@gmail.com>
2023-03-18 21:43:03 +0000

    added gradius decklist and extrav resolve

--- PluginExecutors/ABCGradius/ABCGradius.cs
+++ PluginExecutors/ABCGradius/ABCGradius.cs
@@ -7,12 +7,25 @@ using YGOSharp.OCGWrapper.Enums;
 
 namespace WindBot.Game.AI.Decks
 {
-    [Deck("Sample", "AI_Sample")]
+    [Deck("ABCGradius", "AI_ABCGradius")]
     public class SampleExecutor : DefaultExecutor
     {
+        public enum CardID : int
+        {
+            PotOfExtravagance = 49238328,
+        }
         public SampleExecutor(GameAI ai, Duel duel)
             : base(ai, duel)
         {
+            //Shotguns
+            AddExecutor(ExecutorType.Activate, (int)CardID.PotOfExtravagance, POEeffect);
+        }
+
+        public bool POEeffect()
+        {
+            //always try and draw 2
+            AI.SelectOption(1);
+            return true;
         }
     }
 }
